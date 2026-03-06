d6069d0f2d1d7dcf4432a3eda7044183d509c342
Raj23rk <kumarrk23dev@gmail,com>
2026-03-06 21:25:23 +0530

    backend api url added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api" || "https://career-app-kwbn.vercel.app/api"
+  baseURL: "http://localhost:5000/api" || "https://career-app-ph4c.onrender.com/api"
 });
 
 export default API;
